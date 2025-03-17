ef2805c51c84b1beba9d27f8d1f604e428ff156a
Dat3BE <dat3be@gmail.com>
2025-03-17 23:14:11 +0700

    something new

--- lib/widgets/banner_ad_widget.dart
+++ lib/widgets/banner_ad_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_mobile_ads/google_mobile_ads.dart';
 import 'package:tikup/services/ad_service.dart';
+import 'package:tikup/utils/ad_helper.dart';
 
 class BannerAdWidget extends StatefulWidget {
   final double maxHeight;
@@ -18,6 +19,8 @@ class BannerAdWidget extends StatefulWidget {
 
 class _BannerAdWidgetState extends State<BannerAdWidget> {
   final AdService _adService = AdService();
+  BannerAd? _bannerAd;
+  bool _isAdLoaded = false;
   bool _isAdLoading = true;
   int _retryAttempt = 0;
   static const int _maxRetries = 3;
@@ -28,6 +31,12 @@ class _BannerAdWidgetState extends State<BannerAdWidget> {
     _loadAd();
   }
   
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
+  }
+  
   void _loadAd() {
     // Don't load ads for premium users
     if (_adService.isPremium) return;
@@ -36,34 +45,65 @@ class _BannerAdWidgetState extends State<BannerAdWidget> {
       _isAdLoading = true;
     });
     
-    if (!_adService.isBannerAdReady) {
-      _adService.loadBannerAd();
-      
-      // Check after a delay to trigger UI refresh if ad loaded
-      Future.delayed(const Duration(seconds: 1), () {
-        if (mounted) {
-          setState(() {
-            _isAdLoading = false;
-          });
-          
-          // If ad failed to load and we haven't exceeded max retries, try again
-          if (!_adService.isBannerAdReady && _retryAttempt < _maxRetries) {
-            _retryAttempt++;
-            _loadAd();
-          }
-        }
-      });
-    } else {
-      setState(() {
-        _isAdLoading = false;
-      });
+    _loadBannerWithSize(AdSize.banner);
+  }
+  
+  void _loadBannerWithSize(AdSize adSize) {
+    if (_bannerAd != null) {
+      _bannerAd!.dispose();
+      _bannerAd = null;
     }
+    
+    _bannerAd = BannerAd(
+      adUnitId: AdHelper.bannerAdUnitId,
+      size: adSize,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (ad) {
+          debugPrint('Banner ad loaded successfully with size: ${adSize.width}x${adSize.height}');
+          if (mounted) {
+            setState(() {
+              _isAdLoaded = true;
+              _isAdLoading = false;
+            });
+          }
+        },
+        onAdFailedToLoad: (ad, error) {
+          debugPrint('Banner ad failed to load: ${error.message}');
+          debugPrint('Banner ad error code: ${error.code}');
+          ad.dispose();
+          _bannerAd = null;
+          
+          if (mounted) {
+            setState(() {
+              _isAdLoaded = false;
+              _isAdLoading = false;
+            });
+          }
+          
+          // If banner ad fails, try with a different size
+          if (adSize == AdSize.banner && _retryAttempt < _maxRetries) {
+            _retryAttempt++;
+            debugPrint('Trying with medium rectangle size instead');
+            _loadBannerWithSize(AdSize.mediumRectangle);
+          } else if (adSize == AdSize.mediumRectangle && _retryAttempt < _maxRetries) {
+            _retryAttempt++;
+            debugPrint('Trying with large banner size instead');
+            _loadBannerWithSize(AdSize.largeBanner);
+          }
+        },
+        onAdOpened: (ad) => debugPrint('Banner ad opened'),
+        onAdClosed: (ad) => debugPrint('Banner ad closed'),
+      ),
+    );
+    
+    _bannerAd!.load();
   }
   
   @override
   Widget build(BuildContext context) {
     // If user is premium or no ad is loaded, return a minimal container
-    if (_adService.isPremium || (!_adService.isBannerAdReady && !_isAdLoading) || _adService.bannerAd == null) {
+    if (_adService.isPremium || (!_isAdLoaded && !_isAdLoading) || _bannerAd == null) {
       return Container(
         width: double.infinity,
         height: 0, // No height when no ad is loaded or user is premium
@@ -90,8 +130,8 @@ class _BannerAdWidgetState extends State<BannerAdWidget> {
     }
     
     // Get the ad's width and height
-    final double adWidth = _adService.bannerAd!.size.width.toDouble();
-    final double adHeight = _adService.bannerAd!.size.height.toDouble();
+    final double adWidth = _bannerAd!.size.width.toDouble();
+    final double adHeight = _bannerAd!.size.height.toDouble();
     
     // Calculate the height - capped at maxHeight
     final double height = adHeight > widget.maxHeight ? widget.maxHeight : adHeight;
@@ -114,7 +154,7 @@ class _BannerAdWidgetState extends State<BannerAdWidget> {
           width: adWidth,
           height: adHeight,
           alignment: Alignment.center,
-          child: AdWidget(ad: _adService.bannerAd!),
+          child: AdWidget(ad: _bannerAd!),
         ),
       ),
     );
